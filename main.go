a1ad1e1f1af1bc3b7c441ad97e5437c829a468eb
Mathias Haugen <haugen1997@gmail.com>
2023-04-12 18:51:43 +0200

    forsøk

--- main.go
+++ main.go
@@ -3,32 +3,61 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
-	yr "/minyr/yr/yr"
+	"strings"
+
+	"github.com/mathhaug/minyr/yr"
 )
 
 func main() {
-	src, err := os.Open("/minyr/kjevik-temp-celsius-20220318-20230318.csv")
-	if err != nil {
-		log.Fatal(err)
+	// Venter på at brukeren skal skrive inn "minyr"
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Skriv inn 'minyr' for å starte programmet: ")
+	text, _ := reader.ReadString('\n')
+	if strings.ToLower(strings.TrimSpace(text)) != "minyr" {
+		fmt.Println("Ugyldig verdi.")
+		return
 	}
-	defer src.Close()
-	log.Println(src)
 
-	var input string
-	scanner := bufio.NewScanner(os.Stdin)
+	// Viser brukeren en meny med valg
+	fmt.Println("Valg:")
+	fmt.Println("  - 'convert' for å konvertere tempraturen fra Celsius til Fahrenheit")
+	fmt.Println("  - 'average' for å begregne gjennomsnitt tempratur for perioden")
+	fmt.Println("Skriv 'q' eller 'quit' for å avbryte.")
+	for {
+		fmt.Print("Velg: ")
+		option, _ := reader.ReadString('\n')
+		option = strings.ToLower(strings.TrimSpace(option))
 
-	for scanner.Scan() {
-		input = scanner.Text()
-		if input == "q" || input == "exit" {
-			fmt.Println("exit")
-			os.Exit(0)
-		} else if input == "convert" {
-			fmt.Println("Konverterer alle maalingene gitt i grader Celsius til grader Fahrenheit.")
-		} else {
-			fmt.Println("Vennligst velg convert, average eller exit")
+		if option == "convert" {
+			err := yr.Convert()
+			if err != nil {
+				fmt.Println("Feil med begregning av gjennomsnitt tempratur:", err)
+				return
+			}
+			fmt.Println("Gjennomsnitt begregning fullført.")
+			break
 		}
+
+		if option == "average" {
+			fmt.Print("Velg enhet for begregning ('c' for Celsius eller 'f' for Fahrenheit): ")
+			unit, _ := reader.ReadString('\n')
+			unit = strings.ToLower(strings.TrimSpace(unit))
+
+			avg, err := yr.Average(unit)
+			if err != nil {
+				fmt.Println("Feil under kalkulasjon:", err)
+				return
+			}
+			fmt.Printf("Gjennomsnittlig tempratur: %.2f %s\n", avg, unit)
+			break
+		}
+
+		if option == "q" || option == "quit" {
+			fmt.Println("Avbryt program.")
+			return
+		}
+
+		fmt.Println("Ugylid verdi, prøv igjen.")
 	}
 }
-
